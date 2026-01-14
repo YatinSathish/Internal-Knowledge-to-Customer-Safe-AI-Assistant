0aa392818ddf26f773ab5a6d25f193902949a87b
Yatindran Sathishkumar <yatinsat@gmail.com>
2026-01-14 23:26:42 +1100

    Ingestion logic- added missing keys handling logic

--- src/ingestion.py
+++ src/ingestion.py
@@ -6,14 +6,14 @@ def ingest(json_path):
     with open(json_path, "r") as f:
         data = json.load(f)
     threads = []
-    for channel in data.get('channels'):
+    for channel in data.get('channels', []):
         channel_name = channel['name']
 
-        for thread in channel.get('threads'):
+        for thread in channel.get('threads', []):
             messages = []
             thread_id = thread['id']
 
-            for mesg in thread.get('messages'):
+            for mesg in thread.get('messages', []):
                 messages.append(
                     SlackMessage(
                         author=mesg['author'],
